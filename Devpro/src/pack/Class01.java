085f309527915e44df6b0b6cd421917bf469fd1d
AdmiN <AdmiN@ITWS0042>
2024-08-29 10:29:07 +0530

    Teamlead Commit

---
+++ Devpro/src/pack/Class01.java
@@ -0,0 +1,27 @@
+package pack;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("teamlead");
+
+	}
+	
+	public void m1()
+	{
+		//542
+	}
+	
+	public void m2()
+	{
+		//540
+	}
+	
+	public void m3()
+	{
+		//541
+	}
+
+}
